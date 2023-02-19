3a8380577225f61ce33bc8fac23ece562611f755
abhijit navale <abhijitnwork@gmail.com>
2023-02-19 18:14:17 +0530

    basic devise login and registration flow

--- app/controllers/api/v1/referrals_controller.rb
+++ app/controllers/api/v1/referrals_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::ReferralsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:welcome]
 
+  def welcome; end
   def index; end
 end
