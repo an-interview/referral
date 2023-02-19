f78904739cfc41b945ea89a9e668969192826d17
abhijit navale <abhijitnwork@gmail.com>
2023-02-19 18:00:20 +0530

    disable custom devise controllers since react is disabled

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users,
-             controllers: {
-                 sessions: 'sessions',
-                 registrations: 'registrations'
-             }
+  devise_for :users
+             # controllers: {
+             #     sessions: 'sessions',
+             #     registrations: 'registrations'
+             # }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
